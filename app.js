248b8d9d853a191c1e3df7a2ead099404c948f12
bcturner <bcturner@gmail.com>
2017-03-29 14:49:04 -0400

    cleared app.js of all routes from previous code

--- app.js
+++ app.js
@@ -11,8 +11,8 @@ const ROOT = "./public_html";
 
 // create http server
 var server = http.createServer(handleRequest); 
-server.listen(process.env.PORT || 2406);
-console.log('Server listening on port 2406');
+server.listen(process.env.PORT || 2000);
+console.log('Server listening on port 2000');
 
 var users = {};
 
@@ -25,58 +25,19 @@ function handleRequest(req, res) {
 	var urlObj = url.parse(req.url,true);
 	var filename = ROOT+urlObj.pathname;
 	
-	if(urlObj.pathname==="/memory/intro"){
-		console.log("new User: "+urlObj.query.username);
+	//the callback sequence for static serving...
+	fs.stat(filename,function(err, stats){
+		if(err){   //try and open the file and handle the error, handle the error
+			respondErr(err);
+		}else{
+			if(stats.isDirectory())	filename+="/index.html";
 		
-		
-		client = {pairs:board.makeBoard(4)};
-
-		users[urlObj.query.username] = client;
-
-		console.log(users[urlObj.query.username].pairs);
-		var usersJSON = JSON.stringify(users);
-		res.writeHead(200);
-		
-		res.end(usersJSON);
-
-	}
-	else if(urlObj.pathname==="/memory/card"){
-		console.log("user : "+urlObj.query.username);
-		console.log("row : "+urlObj.query.rowindex);
-		console.log("column : "+urlObj.query.columnindex);
-
-		//Create a return package with the value, the row and column co-ordinate
-		var returnPackage = {};
-		valueInt = users[urlObj.query.username].pairs[urlObj.query.rowindex][urlObj.query.columnindex];
-
-		returnPackage["value"] = valueInt;
-		returnPackage["row"] = urlObj.query.rowindex;
-		returnPackage["column"] = urlObj.query.columnindex
-
-		console.log(returnPackage);
-		
-
-		var valueJSON = JSON.stringify(returnPackage);
-		res.writeHead(200);
-		
-		res.end(valueJSON);
-
-	}
-	else{
-		//the callback sequence for static serving...
-		fs.stat(filename,function(err, stats){
-			if(err){   //try and open the file and handle the error, handle the error
-				respondErr(err);
-			}else{
-				if(stats.isDirectory())	filename+="/index.html";
-			
-				fs.readFile(filename,"utf8",function(err, data){
-					if(err)respondErr(err);
-					else respond(200,data);
-				});
-			}
-		});
-	}		
+			fs.readFile(filename,"utf8",function(err, data){
+				if(err)respondErr(err);
+				else respond(200,data);
+			});
+		}
+	});		
 	
 	//locally defined helper function
 	//serves 404 files 
