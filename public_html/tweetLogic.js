dedbf047a2b9ec92b5a6aabe84cb2d8547ed92f8
bcturner <bcturner@gmail.com>
2017-03-29 14:10:11 -0400

    added package for heroku deployment

--- public_html/tweetLogic.js
+++ public_html/tweetLogic.js
@@ -1,8 +1,25 @@
+	$.ajax({method:"GET",
+				url: "/recipe/"+name,
+				dataType:"json",
+				success: function(data){
+					console.log("Loading recipe into form: ",data);
+					
+					//fill in form		
+					$("#recipeName").val(data.name.split("_").join(" "));
+					$("#recipeDuration").val(data.duration);
+					$("#recipeIngredients").val(data.ingredients);
+					$("#recipeDirections").val(data.directions);
+					$("#recipeNotes").val(data.notes);
+				},
+				cache:false
+	});
+
+
 	var user = prompt("Please enter twitter username")
 	
 	$( document ).ready(function() {
-		document.title = "mitter - "+user
-		document.getElementById("header").innerHTML = "simple twitter viewer - @"+user;
+		document.title = "mitter - @"+user
+		document.getElementById("header").innerHTML = "mitter - the minimalist twitter viewer - @"+user;
 	});
 
 
